e4661137bdf14272d0239d89d8944e4e4af91c1e
Riyashetye4101 <84229593+Riyashetye4101@users.noreply.github.com>
2022-03-01 16:50:43 +0530

    updated mode

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -1,93 +1,100 @@
 import cv2
-import mediapipe as mp
+import numpy as np
+import handtracking_module as htm
 import time
+import autopy
+import mouse
+cap=cv2.VideoCapture(0)
+width, height=640,480
+cap.set(3,width)
+cap.set(4, height)
+
+# steps:-
+'''
+1. find the hand landmarks
+2. set the tip of the index and middle fingers
+3. check which fingers are up
+4. only index finger: Moving mode
+5. convert coordinates
+6. smoothing so that flickering is not happend
+7. move the mouse
+8. both index and middle fingers are up: clicking mode
+9. find the distance between the fingers
+10. click mouse if distance short
+11. frame rate
+12. display
+'''
+
+smooth = 5
+prevx, prevy= 0,0
+currx, curry=0,0
+detector = htm.Detection(maxHands=1)
+ptime=0
+frameR = 100 # frame reduction
+screen_w, screen_h = autopy.screen.size()
+while True:
+	#1
+	success, img=cap.read()
+	img = detector.findHands(img)
+	lmList, bbox = detector.findPosition(img)
+	#2
+	if len(lmList)!=0:
+		x1,y1=lmList[8][1:]
+		x2,y2=lmList[12][1:]
+
+		#3 
+		fingers=detector.fingersUp()
+		print(fingers)
+		cv2.rectangle(img,(frameR,frameR),(width-frameR,height-frameR),(255,0,255),2)
+		#4 only index finger:  moving move
+		if fingers[1]==1 and fingers[2]==0 :
+
+			#5 convert coordinates
+			
+			x3=np.interp(x1, (frameR,width-frameR),(0,screen_w))
+			y3=np.interp(y1, (frameR,height-frameR),(0,screen_h))
+
+			currx=prevx+(x3-prevx)/smooth
+			curry=prevy+(y3-prevy)/smooth
+			autopy.mouse.move(screen_w-currx,curry)
+			# scale = autopy.screen.scale()
+			# autopy.mouse.smooth_move((screen_w-x3)/scale,(screen_h-y3)/scale)
+
+			cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FILLED)
+			prevx, prevy = currx, curry
+
+		# both index and middle fingers are up: clicking mode
+		if fingers[1]==1 and fingers[2]==1:
+			length, img, infoline = detector.findDistance(8,12,img)
+			# print(length)
+			if length<40:
+				cv2.circle(img,(infoline[4],infoline[5]),15,(0,255,0),cv2.FILLED)
+				autopy.mouse.click()
+
+		if fingers[1]==1 and fingers[2]==1 and fingers[3]==1:
+			length, img, infoline = detector.findDistance(8,12,img)
+			print(length)
+			if length<40:
+				cv2.circle(img,(infoline[4],infoline[5]),15,(0,255,0),cv2.FILLED)
+				autopy.mouse.click()
 
 
-class Detection():
-	def __init__(self, mode=False, maxHands=2,modelC=1, detectionCon=0.5, trackCon=0.5):
-		self.mode = mode
-		self.modelC = modelC
-		self.maxHands=maxHands
-		self.detectionCon = detectionCon
-		self.trackCon = trackCon
-
-		self.mpDraw = mp.solutions.drawing_utils
-		self.mpHands = mp.solutions.hands
-		self.hands = self.mpHands.Hands(
-			self.mode,
-			self.maxHands,
-			self.modelC,
-			self.detectionCon,
-			self.trackCon
-			)
-
-	def findHands(self, img, draw=True):
-														# sending the rgb color images
-		imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-		self.results =self.hands.process(imgRGB)
-														# print(results.multi_hand_landmarks)
-														# if the hand is detected then do some work
-		if self.results.multi_hand_landmarks:
-		    for handlms in self.results.multi_hand_landmarks:
-		    	if draw:
-		        	self.mpDraw.draw_landmarks(img,handlms,self.mpHands.HAND_CONNECTIONS)
-
-		return img
-
-	def findPosition(self, img, handNo=0, draw = True):
-		lm_List = []
-		
-		if self.results.multi_hand_landmarks:
-			myHand = self.results.multi_hand_landmarks[handNo]
-			for id,lm in enumerate(myHand.landmark):
-			    # print(id,' ',lm)
-			    h, w, c= img.shape
-			    cx, cy = int(lm.x*w), int(lm.y*h)  # provides the pixels of each hand frame
-			  
-			    lm_List.append([id,cx,cy])
-			    
-			    if draw:
-			    	cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
-
-		return lm_List
 
 
 
 
 
+	#11
+	ctime = time.time()
+	fps=1/(ctime-ptime)
+	ptime=ctime
+	cv2.putText(
+    	img, str(int(fps)),
+    	(20,50),
+    	cv2.FONT_HERSHEY_PLAIN,
+    	3,(255,9,6), 3
+    	)
 
-
-
-
-
-
-
-
-
-
-
-def main():
-	ptime=0
-	ctime=0
-	detector = Detection()
-	cap = cv2.VideoCapture(0)
-
-	while True:
-		success, img = cap.read()
-		img = detector.findHands(img)
-		lmlist = detector.findPosition(img)
-		if len(lmlist)!=0:
-			print(lmlist[4])
-
-		ctime=time.time()
-		fps = 1/(ctime-ptime)
-		ptime = ctime
-
-		cv2.putText(img,str(int(fps)),(10,78),cv2.FONT_HERSHEY_PLAIN,3,
-		            (255,0,255),3)
-
-		cv2.imshow("frame" , img)
-		cv2.waitKey(1)
-
-if __name__ == '__main__':
-	main()
+    #12
+	cv2.imshow("img",img)
+	cv2.waitKey(1)
